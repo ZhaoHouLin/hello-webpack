f0babcab574ea57e08fe0c8f6283414be24fed47
RaidenLin <rodes5292@gmail.com>
2020-02-20 16:32:25 +0800

    webpack初始環境建置

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path')
+
+console.log("__dirname:",__dirname)
+console.log("resolve:",path.resolve(__dirname,'./src'))
+console.log(process.env.NODE_ENV)
+
+module.exports = {
+    context: path.resolve(__dirname,'./src'),
+    //path.resolve(當前目錄相對路徑,絕對路徑) 存放進入點檔案的資料夾路徑
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname,'./dist'), 
+        filename: 'index-bundle.js',
+    }
+}
