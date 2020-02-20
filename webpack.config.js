b9f0a3d5f4edad10d26c065ed4e1c366ed309939
RaidenLin <rodes5292@gmail.com>
2020-02-20 16:57:39 +0800

    注入路徑修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
 const path = require('path')
 
-console.log("__dirname:",__dirname)
-console.log("resolve:",path.resolve(__dirname,'./src'))
-console.log(process.env.NODE_ENV)
+// console.log("__dirname:",__dirname)
+// console.log("resolve:",path.resolve(__dirname,'./src'))
+// console.log(process.env.NODE_ENV)
 
 module.exports = {
-    context: path.resolve(__dirname,'./src'),
+    context: path.resolve(__dirname,'src'),
     //path.resolve(當前目錄相對路徑,絕對路徑) 存放進入點檔案的資料夾路徑
-    entry: './index.js',
+    entry: {
+        index: './js/index.js',
+    },
     output: {
-        path: path.resolve(__dirname,'./dist'), 
-        filename: 'index-bundle.js',
+        path: path.resolve(__dirname,'dist'), 
+        filename: './js/[name].js',
     }
 }
